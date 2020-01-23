2a2d286b110485e39f8d2390a634a93078346f2a
Adam Cook <me@cookadam.co.uk>
2020-01-23 11:42:00 +0000

    Updated profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1827,6 +1827,40 @@ function Set-Secure {
     $credential | Export-CliXml $path
 }
 
+Function New-ModuleDirStructure {
+    Param (
+        [Parameter(Mandatory)]
+        [String]$Path,
+        [Parameter(Mandatory)]
+        [String]$ModuleName,
+        [Parameter(Mandatory)]
+        [String]$Author,
+        [Parameter(Mandatory)]
+        [String]$Description
+    )
+
+    # Create the module and private function directories
+    mkdir $Path\$ModuleName
+    mkdir $Path\$ModuleName\Private
+    mkdir $Path\$ModuleName\Public
+    mkdir $Path\$ModuleName\en-US # For about_Help files
+    mkdir $Path\Tests
+
+    #Create the module and related files
+    New-Item "$Path\$ModuleName\$ModuleName.psm1" -ItemType File
+    New-Item "$Path\$ModuleName\$ModuleName.Format.ps1xml" -ItemType File
+    New-Item "$Path\$ModuleName\en-US\about_$ModuleName.help.txt" -ItemType File
+    New-Item "$Path\Tests\$ModuleName.Tests.ps1" -ItemType File
+    New-ModuleManifest -Path $Path\$ModuleName\$ModuleName.psd1 `
+                    -RootModule $ModuleName.psm1 `
+                    -Description $Description `
+                    -PowerShellVersion 3.0 `
+                    -Author $Author `
+                    -FormatsToProcess "$ModuleName.Format.ps1xml"
+
+    # Copy the public/exported functions into the public folder, private functions into private folder
+}
+
 Function Get-MyCommands {
     Get-Content -Path $profile.CurrentUserAllHosts | Select-String -Pattern "^function.+" | ForEach-Object {
         [Regex]::Matches($_, "^function ([a-z.-]+)","IgnoreCase").Groups[1].Value
