15746a5410428cbb86e9dcf9e54288fbe41317dc
Adam Cook <me@cookadam.co.uk>
2020-01-23 11:53:31 +0000

    Updated profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1828,6 +1828,10 @@ function Set-Secure {
 }
 
 Function New-ModuleDirStructure {
+    <#
+    .NOTES
+        http://ramblingcookiemonster.github.io/Building-A-PowerShell-Module/
+    #>
     Param (
         [Parameter(Mandatory)]
         [String]$Path,
@@ -1836,27 +1840,32 @@ Function New-ModuleDirStructure {
         [Parameter(Mandatory)]
         [String]$Author,
         [Parameter(Mandatory)]
-        [String]$Description
+        [String]$Description,
+        [Parameter()]
+        [String]$PowerShellVersion = 5.1
     )
 
     # Create the module and private function directories
-    mkdir $Path\$ModuleName
-    mkdir $Path\$ModuleName\Private
-    mkdir $Path\$ModuleName\Public
-    mkdir $Path\$ModuleName\en-US # For about_Help files
-    mkdir $Path\Tests
+    New-Item -Path $Path\$ModuleName -ItemType Directory -Force
+    New-Item -Path $Path\$ModuleName\Private -ItemType Directory -Force
+    New-Item -Path $Path\$ModuleName\Public -ItemType Directory -Force
+    New-Item -Path $Path\$ModuleName\en-US -ItemType Directory -Force # For about_Help files
+    #New-Item -Path $Path\Tests -ItemType Directory -Force
 
     #Create the module and related files
-    New-Item "$Path\$ModuleName\$ModuleName.psm1" -ItemType File
-    New-Item "$Path\$ModuleName\$ModuleName.Format.ps1xml" -ItemType File
-    New-Item "$Path\$ModuleName\en-US\about_$ModuleName.help.txt" -ItemType File
-    New-Item "$Path\Tests\$ModuleName.Tests.ps1" -ItemType File
-    New-ModuleManifest -Path $Path\$ModuleName\$ModuleName.psd1 `
-                    -RootModule $ModuleName.psm1 `
-                    -Description $Description `
-                    -PowerShellVersion 3.0 `
-                    -Author $Author `
-                    -FormatsToProcess "$ModuleName.Format.ps1xml"
+    New-Item "$Path\$ModuleName\$ModuleName.psm1" -ItemType File -Force
+    New-Item "$Path\$ModuleName\$ModuleName.Format.ps1xml" -ItemType File -Force
+    New-Item "$Path\$ModuleName\en-US\about_$ModuleName.help.txt" -ItemType File -Force
+    #New-Item "$Path\Tests\$ModuleName.Tests.ps1" -ItemType File -Force
+    $NewModuleManifestSplat = @{
+        Path                = Join-Path -Path $Path -ChildPath $ModuleName | Join-Path -ChildPath "$ModuleName.psd1"
+        RootModule          = $ModuleName.psm1
+        Description         = $Description
+        PowerShellVersion   = $PowerShellVersion
+        Author              = $Author
+        FormatsToProcess    = "$ModuleName.Format.ps1xml"
+    }
+    New-ModuleManifest @NewModuleManifestSplat
 
     # Copy the public/exported functions into the public folder, private functions into private folder
 }
