54d9cd201d4771e8978b7e922197a761e74b300e
Adam Cook <me@cookadam.co.uk>
2020-02-03 10:15:40 +0000

    Updated profile.ps1 - Get-Dns

--- profile.ps1
+++ profile.ps1
@@ -1619,6 +1619,44 @@ Function New-RebootScheduledTask {
     Register-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath -InputObject $Task -User "System" -CimSession $Session
 }
 
+Function Get-Dns {
+    Param( 
+        [Parameter()]
+        [String[]]$ComputerName,
+        [Parameter(Mandatory)]
+        [String]$FirstOctet,
+        [Parameter()]
+        [PSCredential]$Credential
+    )
+    $InvokeCommandSplat = @{
+        ScriptBlock = {
+            $InterfaceIndex = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress.StartsWith($FirstOctet) } | Select-Object -ExpandProperty InterfaceIndex
+            [PSCustomObject]@{
+                ComputerName = $env:COMPUTERNAME
+                DNSAddresses = [String]::Join(", ", (Get-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -AddressFamily IPv4).ServerAddresses)
+            }
+        }
+    }
+    if ($PSBoundParameters.ContainsKey("Credential")) {
+        $InvokeCommandSplat["Credential"] = $Credential
+    }
+    $Jobs = ForEach ($Computer in $ComputerName) {
+        if ($PSBoundParameters.ContainsKey("ComputerName")) {
+            $InvokeCommandSplat["ComputerName"] = $Computer
+        }
+        Invoke-Command @InvokeCommandSplat -AsJob
+    }
+    while (Get-Job -State "Running") {
+        $TotalJobs = $Jobs.count
+        $NotRunning = $Jobs | Where-Object { $_.State -ne "Running" }
+        $Running = $Jobs | Where-Object { $_.State -eq "Running" }
+        Write-Progress -Activity "Waiting on results" -Status "$($TotalJobs-$NotRunning.count) Jobs Remaining: $($Running.Location)" -PercentComplete ($NotRunning.count/(0.1+$TotalJobs) * 100)
+        Start-Sleep -Seconds 2
+    }
+    Get-Job | Receive-Job
+    Get-Job | Remove-Job    
+}
+
 Function WmiExec {
     <#        
         .SYNOPSIS
