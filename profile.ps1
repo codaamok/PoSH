fbe02dd9b93ddae5e5147e0b19bd491e288c20b0
Adam Cook <me@cookadam.co.uk>
2020-04-01 11:19:16 +0100

    Added Get-Syntax <3

--- profile.ps1
+++ profile.ps1
@@ -2211,6 +2211,131 @@ function Remove-RemotePSSession {
     }
 }
 
+function Get-CommandInfo {
+    <#
+    .SYNOPSIS
+        Get-Command helper.
+    .DESCRIPTION
+        Get-Command helper.
+    .NOTES
+        https://github.com/indented-automation/Indented.Profile/blob/master/Indented.Profile/public/Get-CommandInfo.ps1
+    #>
+
+    [CmdletBinding()]
+    param (
+        # The name of a command.
+        [Parameter(Mandatory, ParameterSetName = 'ByName')]
+        [String]$Name,
+
+        # A CommandInfo object.
+        [Parameter(Mandatory, ParameterSetName = 'FromCommandInfo')]
+        [System.Management.Automation.CommandInfo]$CommandInfo,
+
+        # If a module name is specified the private / internal scope of the module will be searched.
+        [String]$ModuleName,
+
+        # Claims and discards any other supplied arguments.
+        [Parameter(ValueFromRemainingArguments, DontShow)]
+        $EaterOfArgs
+    )
+
+    if ($Name) {
+        if ($ModuleName) {
+            try {
+                if (-not ($moduleInfo = Get-Module $ModuleName)) {
+                    $moduleInfo = Import-Module $ModuleName -Global -PassThru
+                }
+                $CommandInfo = & $moduleInfo ([ScriptBlock]::Create('Get-Command {0}' -f $Name))
+            } catch {
+                $pscmdlet.ThrowTerminatingError($_)
+            }
+        } else {
+            $CommandInfo = Get-Command -Name $Name
+        }
+    }
+
+    if ($CommandInfo -is [System.Management.Automation.AliasInfo]) {
+        $CommandInfo = $CommandInfo.ResolvedCommand
+    }
+
+    return $CommandInfo
+}
+
+function Get-Syntax {
+    <#
+    .SYNOPSIS
+        Get the syntax for a command.
+    .DESCRIPTION
+        Get the syntax for a command. A wrapper for Get-Command -Syntax.
+    .NOTES
+        https://github.com/indented-automation/Indented.Profile/blob/master/Indented.Profile/public/Get-Syntax.ps1
+    #>
+
+    [CmdletBinding()]
+    [Alias('synt', 'syntax')]
+    param (
+        # The name of a command.
+        [Parameter(Mandatory, Position = 1, ValueFromPipelineByPropertyName, ParameterSetName = 'ByName')]
+        [String]$Name,
+
+        # A CommandInfo object.
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'FromCommandInfo')]
+        [System.Management.Automation.CommandInfo]$CommandInfo,
+
+        # Write syntax in the short format used by Get-Command.
+        [Switch]$Short
+    )
+
+    begin {
+        $commonParams = @(
+            [System.Management.Automation.Internal.CommonParameters].GetProperties().Name
+            [System.Management.Automation.Internal.ShouldProcessParameters].GetProperties().Name
+            [System.Management.Automation.Internal.TransactionParameters].GetProperties().Name
+        )
+    }
+
+    process {
+        $CommandInfo = Get-CommandInfo @psboundparameters
+        foreach ($parameterSet in $CommandInfo.ParameterSets) {
+            if ($Short) {
+                "`n{0} {1}" -f $CommandInfo.Name, $parameterSet
+            } else {
+                $stringBuilder = [System.Text.StringBuilder]::new().AppendFormat('{0} ', $commandInfo.Name)
+
+                $null = foreach ($parameter in $parameterSet.Parameters) {
+                    if ($parameter.Name -notin $commonParams) {
+                        if (-not $parameter.IsMandatory) {
+                            $stringBuilder.Append('[')
+                        }
+
+                        if ($parameter.Position -gt [Int32]::MinValue) {
+                            $stringBuilder.Append('[')
+                        }
+
+                        $stringBuilder.AppendFormat('-{0}', $parameter.Name)
+
+                        if ($parameter.Position -gt [Int32]::MinValue) {
+                            $stringBuilder.Append(']')
+                        }
+
+                        if ($parameter.ParameterType -ne [Switch]) {
+                            $stringBuilder.AppendFormat(' <{0}>', $parameter.ParameterType.Name)
+                        }
+
+                        if (-not $parameter.IsMandatory) {
+                            $stringBuilder.Append(']')
+                        }
+
+                        $stringBuilder.AppendLine().Append(' ' * ($commandInfo.Name.Length + 1))
+                    }
+                }
+
+                $stringBuilder.AppendLine().ToString()
+            }
+        }
+    }
+}
+
 function ConvertTo-HexString {
     [CmdletBinding()]
     param (
